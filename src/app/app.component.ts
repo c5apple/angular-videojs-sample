1d35427dbc620ba65219913babea3b27f4cb5614
c5apple <c5apple_party@yahoo.co.jp>
2020-03-28 18:46:19 +0900

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import videojs from "video.js";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-videojs-sample';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    var player = videojs("test", {
+      html5: {
+        hls: {
+          withCredentials: true
+        }
+      },
+      flash: {
+        hls: {
+          withCredentials: true
+        }
+      }
+    });
+    // player.play();
+  }
+
 }
